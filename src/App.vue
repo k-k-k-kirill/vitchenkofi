7d2ab6037b608a869395c7254de9a702d3d8ce9e
Kirill Vitchenko <vitchenko.kirill@gmail.com>
2020-08-08 21:21:17 +0300

    styles and vanta js

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
   <div id="app">
-
+    <h1>Welcome to the future!</h1>
   </div>
 </template>
 
 <script>
+import * as THREE from './assets/scripts/three.r95.min' // eslint-disable-line
+
 export default {
-  name: 'App'
+  name: 'App',
+  mounted() {
+    window.VANTA.WAVES({
+      el: "html",
+      mouseControls: true,
+      touchControls: true,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      THREE: THREE,
+      color: 0x0,
+      shininess: 12,
+      waveSpeed: 0.25,
+      zoom: 0.75
+    })
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './assets/styles/main.scss'
+import * as vanta from './assets/scripts/vanta.waves.min' // eslint-disable-line
 
 Vue.config.productionTip = false
 
