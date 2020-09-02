7d7142d166ec6336cae95969ed62d9b6d1edbe26
Kirill Vitchenko <vitchenko.kirill@gmail.com>
2020-09-02 08:42:49 +0300

    prismic integration

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './assets/styles/main.scss'
+import PrismicVue from 'prismic-vue';
+import linkResolver from './link-resolver';
 
 // Router
 import VueRouter from 'vue-router'
 import routes from './routes'
+
+	
+Vue.use(PrismicVue, {
+  endpoint: window.prismic.endpoint,
+  linkResolver
+});
 
 Vue.config.productionTip = false
 
